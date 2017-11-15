648feb70ad32d94bb3854bc41dcf8a4209796420
todiadiyatmo <t@tonjoo.id>
2017-11-15 09:36:12 +0700

    languageSwitcher class

--- app/Modules/Plugins/AksaraMultiBas/helper.php
+++ app/Modules/Plugins/AksaraMultiBas/helper.php
@@ -3,13 +3,25 @@ use App\Modules\Plugins\PostType\Model\PostMeta;
 use App\Modules\Plugins\PostType\Model\Post;
 use App\Modules\Plugins\AksaraMultiBas\TranslationEngine;
 
-function get_registered_languages() {
+function get_registered_languages()
+{
     return collect(json_decode(get_options('multibas_countries', "[]"),true));
 }
 
-function get_translated_post($post,$lang) {
+function get_translated_post($post,$lang)
+{
     $translationEngine = new TranslationEngine();
-
     return $translationEngine->getTranslatedPost($post,$lang);
+}
 
+function set_multibas_language($lang=false)
+{
+    $languageSwitcher = \App::make('App\Modules\Plugins\AksaraMultiBas\LanguageSwitcher');
+    return $languageSwitcher->setLanguage($lang);
+}
+
+function get_current_multibas_language()
+{
+    $languageSwitcher = \App::make('App\Modules\Plugins\AksaraMultiBas\LanguageSwitcher');
+    return $languageSwitcher->getCurrentLanguage();
 }
