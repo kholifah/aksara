7e745d9596c1f79f50961812a396609d47e4fc88
todiadiyatmo <t@tonjoo.id>
2017-11-27 12:00:19 +0700

    testing cond

--- app/Modules/Plugins/PostType/FrontEnd.php
+++ app/Modules/Plugins/PostType/FrontEnd.php
@@ -12,10 +12,10 @@ class FrontEnd
         \Eventy::addAction('aksara.routes.front_end',function(){
             $permalink = \App::make('App\Modules\Plugins\PostType\Permalink');
 
-            $permalink->generatePostPermalinkRoutes();
             $permalink->generatePostArchivePermalinkRoutes();
             $permalink->generateSearchRoute();
             $permalink->generateHomeRoute();
+            $permalink->generatePostPermalinkRoutes();
 
             // Catch All Controller
             \Route::get( '{slug}', ['as' => 'aksara.post-type.front-end.single.catch-all', 'uses' =>'\App\Modules\Plugins\PostType\Http\FrontEndController@serve']);
--- app/Modules/Themes/FrontEnd/Aksara/index.php
+++ app/Modules/Themes/FrontEnd/Aksara/index.php
@@ -60,4 +60,21 @@
 
     add_page_template('Contact Form', 'front-end:aksara::page-contact-form');
     add_page_template('About Us', 'front-end:aksara::page-about-us');
+
+    $faqcats = [
+        'label' => [
+            'name' => 'FAQ Kategori'
+       ]
+    ];
+
+    register_taxonomy('faq-category', ['post'], $faqcats);
+
+
+    $galcats = [
+       'label' => [
+           'name' => 'Gallery Kategori'
+       ]
+    ];
+
+    register_taxonomy('gallery-category', ['post'], $galcats);
 });
