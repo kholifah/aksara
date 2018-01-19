ef6b119d769150436813c89775b7e49022cd26de
Arseto Nugroho <satriyo.796@gmail.com>
2018-01-19 10:46:35 +0700

    Fix users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->bollean('active')->default(false);
+            $table->boolean('active')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
