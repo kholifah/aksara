f89c43ab0d0aff019ff75e4d2a30b311f4f6c720
piyo <piyo@tonjoo.com>
2018-03-15 15:53:15 +0700

    translasi lang en multi bahasa

---
+++ app/Modules/Plugins/AksaraMultiBas/resources/lang/en/default.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'multi-lang' => 'Multi Language',
+    'multi-lang-option' => 'Multi Language Option',
+    'add-multi-lang' => 'Add Multi Language',
+    'add-lang' => 'Add Language',
+    'save-lang-setting' => 'Save Setting Language',
+    'registered-lang' => 'Registered Language',
+    'lang' => 'Language',
+    'name' => 'Name',
+    'flag' => 'Flag',
+    'code' => 'Code',
+    'locale' => 'Locale',
+    'default' => 'Default',
+    'action' => 'Action',
+    'delete' => 'Delete',
+    'make-default' => 'Make Default'
+
+];
---
+++ app/Modules/Plugins/AksaraMultiBas/resources/lang/en/message.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'translasi-success-message' => 'Translations successfully added.',
+    'success-message' => 'Data successfully changed.',
+   
+];
