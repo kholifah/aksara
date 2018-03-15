43d9bf310df77919ccb60d365d29b72ff96ecff2
piyo <piyo@tonjoo.com>
2018-03-15 17:02:42 +0700

    translasi lang en plugin menu

---
+++ app/Modules/Plugins/Menu/resources/lang/en/default.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'menu' => 'Menu',
+    'submenu' => 'Sub Menu',
+    'sub-submenu' => 'Sub Sub Menu',
+    'site-menu' => 'Site Menu',
+    'add-menu' => 'Add Menu',
+    'add-submenu' => 'Add Sub Menu',
+    'add-sub-submenu' => 'Add Sub Sub Menu',
+    'save-menu' => 'Save Menu',
+    'label' => 'Label',
+    'classes' => 'Classes',
+    'url' => 'URL',
+    'delete' => 'Delete',
+    'show' => 'Show'
+
+];
---
+++ app/Modules/Plugins/Menu/resources/lang/en/message.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'register-register-menu' => 'Please register the menu with the command `register_menu`.',
+    'menu-updated-message' => 'Menu successfully updated.'
+   
+];
