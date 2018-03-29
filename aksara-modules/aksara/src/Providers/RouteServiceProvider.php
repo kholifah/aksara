19dfe97f029c1c246891280bc0d167143c7b0124
piyo <piyo@tonjoo.com>
2018-03-29 11:54:12 +0700

    update frontend controller n assets

--- aksara-modules/aksara/src/Providers/RouteServiceProvider.php
+++ aksara-modules/aksara/src/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
             aksara_enqueue_style('https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
             aksara_enqueue_style('https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
             aksara_enqueue_style(url("assets/modules-v2/aksara/css/clean-blog.css"));
-            aksara_enqueue_style(url("assets/modules/Plugins/AksaraMultiBas/css/flag-icon.min.css"), "flag-icon" , 25, true);
+            aksara_enqueue_style(url("assets/modules-v2/aksara-multi-bas/css/flag-icon.min.css"), "flag-icon" , 25, true);
             
             // Enqueue Script
             aksara_enqueue_script(url("assets/modules-v2/aksara/vendor/jquery/jquery.min.js"));
@@ -46,8 +46,8 @@ class RouteServiceProvider extends ServiceProvider
             aksara_enqueue_script(url("assets/modules-v2/aksara/js/clean-blog.min.js"));
             aksara_enqueue_script(url("assets/modules-v2/aksara/js/custom.js"));
 
-            add_page_template('Contact Form', 'front-end:aksara::page-contact-form');
-            add_page_template('About Us', 'front-end:aksara::page-about-us');
+            add_page_template('Contact Form', 'aksara::page-contact-form');
+            add_page_template('About Us', 'aksara::page-about-us');
         });
     }
 }
