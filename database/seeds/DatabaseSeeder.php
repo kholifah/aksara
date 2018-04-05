9ae11d6ca53254f21fe2ade6524ae565b5ff8f85
Arseto Nugroho <satriyo.796@gmail.com>
2018-04-05 14:39:22 +0700

    Remove user seeder (module seeder) from core seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-        $this->call(UsersSeeder::class);
+        //call core seeder here
         Model::reguard();
     }
 }
